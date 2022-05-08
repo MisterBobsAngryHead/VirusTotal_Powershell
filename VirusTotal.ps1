d706bebc9b5d13696abae7681b186dd3fff51bc0
RichPrice-scripting <105135379+RichPrice-scripting@users.noreply.github.com>
2022-05-07 20:48:58 -0400

    Add files via upload

---
+++ VirusTotal.ps1
@@ -0,0 +1,31 @@
+﻿
+#request hash
+$Hash = Read-Host "Please enter your MD5 or SHA256 value"
+
+#request api key
+$APIkey = Read-Host "Please enter your VirusTotal API key"
+
+#define header
+$headers=@{}
+$headers.Add("Accept", "application/json")
+$headers.Add("x-apikey", $APIkey)
+
+#api call
+$response = Invoke-WebRequest -Uri "https://www.virustotal.com/api/v3/files/$Hash" -Method GET -Headers $headers
+
+#output status code
+Write-Output "Your status code is " $response.StatusCode
+
+#convert json
+$data = $response.RawContent | ConvertFrom-Json
+Write-Output $data
+
+#test
+if($data.Contains('"malicious": 0'))
+{
+    Write-Output "This file is clean."
+} 
+else
+{
+    Write-Output "This file is malicious."
+}
